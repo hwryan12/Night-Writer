cb63004cc339f3a55574f44da2085133822b1158
Harrison Ryan <hwryan12@gmail.com>
2023-01-14 17:00:58 -0600

    feature: Instaniate an alphabet as a variable in translator.rb and test

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,8 @@
+require_relative "alphabet"
 class Translator
+  attr_reader :alphabet
+  
   def initialize
-    
+    @alphabet = Alphabet.new
   end
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Translator do
     it "exists" do
       expect(translator).to be_instance_of(Translator)
     end
+
+    it "instantiates an English to braille alphabet on creation" do
+      expect(translator.alphabet).to be_instance_of(Alphabet)
+    end
   end
 end
