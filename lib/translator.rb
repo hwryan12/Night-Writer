26416f7de64269815060f9d742ebac74734f8baf
Harrison Ryan <hwryan12@gmail.com>
2023-01-14 17:29:11 -0600

    feature: Add braille array variable and test empty by default

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,16 @@
 require_relative "alphabet"
 class Translator
-  attr_reader :alphabet
-  
-  def initialize
+  attr_reader :alphabet,
+              :message,
+              :array_of_braille
+
+  def initialize(message)
     @alphabet = Alphabet.new
+    @message = message
+    @array_of_braille = []
+  end
+
+  def convert_to_array
+
   end
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 RSpec.describe Translator do
-  let(:translator) { Translator.new }
+  let(:translator) { Translator.new("message") }
   describe "#initialize" do
     it "exists" do
       expect(translator).to be_instance_of(Translator)
@@ -9,6 +9,16 @@ RSpec.describe Translator do
 
     it "instantiates an English to braille alphabet on creation" do
       expect(translator.alphabet).to be_instance_of(Alphabet)
+    end
+  end
+  
+  describe "#convert_to_array" do
+    it "has an empty array of braille characters by default" do
+      expect(translator.array_of_braille).to eq([])
+    end
+    
+    xit "returns the alphabet hash as an array" do
+      expect(translator.convert_to_array).to eq()
     end
   end
 end
