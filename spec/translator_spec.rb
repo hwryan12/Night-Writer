800e7afc98dce647e0596a12dd5383ae6962bc9d
Harrison Ryan <hwryan12@gmail.com>
2023-01-14 12:53:08 -0600

    feature: Create translator initialize test

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,7 +1,10 @@
 require_relative 'spec_helper'
 
 RSpec.describe Translator do
+  let(:translator) { Translator.new }
   describe "#initialize" do
-  
+    it "exists" do
+      expect(translator).to be_instance_of(Translator)
+    end
   end
 end
