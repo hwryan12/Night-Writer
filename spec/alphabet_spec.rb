bf525f64199d44dcf9fb577b7386a09645cd0e77
Harrison Ryan <hwryan12@gmail.com>
2023-01-14 12:47:43 -0600

    feature: Create test to validate hash pairing of eng letters(keys) to braille(values)

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe "Alphabet" do
     it "has an english to braille alphabet as a hash" do
       expect(alphabet.english_alphabet).to be_a(Hash)
     end
+
+    it "has keys of English letters and values of braille letters" do
+      expect(alphabet.english_alphabet["h"]).to eq(["0.", "00", ".."])
+      expect(alphabet.english_alphabet["w"]).to eq([".0", "00", ".0"])
+      expect(alphabet.english_alphabet["r"]).to eq(["0.", "00", "0."])
+    end
   end
 end
