56ef4385a86c19b42f418ffbe9038e2dfd580dc4
Harrison Ryan <hwryan12@gmail.com>
2023-01-15 11:51:57 -0600

    feature: Create braille_translator exists test

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -1,5 +1,11 @@
 require_relative 'spec_helper'
 
 RSpec.describe BrailleTranslator do
-  
+  let(:translator) { BrailleTranslator.new("message") }
+
+  describe "#initalize" do
+    it "exists" do
+      expect(translator).to be_instance_of(BrailleTranslator)
+    end
+  end
 end
