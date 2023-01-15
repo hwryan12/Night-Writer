e30751c4b4ce56fda6fc42af0e174791b0c178f7
Harrison Ryan <hwryan12@gmail.com>
2023-01-14 20:01:17 -0600

    refactor: Refactor night_writer to properly take input and output to braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,12 @@
-input_file = File.read(ARGV[0])
+require_relative 'translator'
 
-translation_to_braille = File.write(ARGV[1], "w")
+english_file = File.open(ARGV[0], 'r')
+text = english_file.read.chomp
+english_file.close
 
-puts "Created #{ARGV[1]} containing #{input_file.length} characters"
+translator = Translator.new(text)
+braille_text = translator.braille_array_to_string(text ,@array_of_braille)
+translation_to_braille = File.open(ARGV[1], 'w')
+translation_to_braille.write(braille_text)
+
+puts "Created #{ARGV[1]} containing #{text.length} characters"
