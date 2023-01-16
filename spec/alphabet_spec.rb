0e14ef4b1695c8785d3dd65d77dc2e76db120521
Harrison Ryan <hwryan12@gmail.com>
2023-01-16 14:50:06 -0600

    refactor: Update alphabet tests with changes made to alphabet.rb being superclass

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -1,24 +1,20 @@
 require_relative 'spec_helper'
 
 RSpec.describe Alphabet do
-  let(:alphabet) { Alphabet.new }
+  let(:alphabet) { Alphabet.new("message") }
   describe "#initialize" do
     it "exists" do
       expect(alphabet).to be_instance_of(Alphabet)
     end
 
     it "has an english to braille alphabet as a hash" do
-      expect(alphabet.english_alphabet).to be_a(Hash)
+      expect(alphabet.alphabet).to be_a(Hash)
     end
 
     it "has keys of English letters and values of braille letters" do
-      expect(alphabet.english_alphabet["h"]).to eq(["0.", "00", ".."])
-      expect(alphabet.english_alphabet["w"]).to eq([".0", "00", ".0"])
-      expect(alphabet.english_alphabet["r"]).to eq(["0.", "00", "0."])
-    end
-
-    it "has braille to english alphabet as a hash" do
-      expect(alphabet.braille_alphabet).to be_a(Hash)
+      expect(alphabet.alphabet["h"]).to eq(["0.", "00", ".."])
+      expect(alphabet.alphabet["w"]).to eq([".0", "00", ".0"])
+      expect(alphabet.alphabet["r"]).to eq(["0.", "00", "0."])
     end
   end
 end
