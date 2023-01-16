3a5109a19bfd6df6dee18f60a66563b9a756710f
Harrison Ryan <hwryan12@gmail.com>
2023-01-16 14:05:25 -0600

    refactor: Refactor #braille_array_to_string to include line helper methods

--- lib/eng_translator.rb
+++ lib/eng_translator.rb
@@ -43,19 +43,9 @@ class EngTranslator
   end
   
   def braille_array_to_string(input)
-    formatted_braile = convert_to_array(input).compact
-    top_line = []
-    formatted_braile.each do |character|
-      top_line << character[0]
-    end
-    middle_line = []
-    formatted_braile.each do |character|
-      middle_line << character[1]
-    end
-    bottom_line = []
-    formatted_braile.each do |character|
-      bottom_line << character[2]
-    end
+    top_line = braille_top_line(input)
+    middle_line = braille_middle_line(input)
+    bottom_line = braille_bottom_line(input)
     place_holder = []
     place_holder << "#{top_line.join}\n#{middle_line.join}\n#{bottom_line.join}"
     place_holder.join
