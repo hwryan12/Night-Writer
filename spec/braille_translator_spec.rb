82282688aba66cf5915880060c9703d4d0d46917
Harrison Ryan <hwryan12@gmail.com>
2023-01-16 14:54:02 -0600

    refactor: Update braille translator alphabet instance test

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -8,8 +8,40 @@ RSpec.describe BrailleTranslator do
       expect(translator).to be_instance_of(BrailleTranslator)
     end
 
-    it "instantiates a braille to English alphabet on creation" do
-      expect(translator.alphabet).to be_instance_of(Alphabet)
+    it "instantiates an inverted alphabet on creation" do
+      expected = {
+        ["0.", "..", ".."] => "a",
+        ["0.", "0.", ".."] => "b",
+        ["00", "..", ".."] => "c",
+        ["00", ".0", ".."] => "d",
+        ["0.", ".0", ".."] => "e",
+        ["00", "0.", ".."] => "f",
+        ["00", "00", ".."] => "g",
+        ["0.", "00", ".."] => "h",
+        [".0", "0.", ".."] => "i",
+        [".0", "00", ".."] => "j",
+        ["0.", "..", "0."] => "k",
+        ["0.", "0.", "0."] => "l",
+        ["00", "..", "0."] => "m",
+        ["00", ".0", "0."] => "n",
+        ["0.", ".0", "0."] => "o",
+        ["00", "0.", "0."] => "p",
+        ["00", "00", "0."] => "q",
+        ["0.", "00", "0."] => "r",
+        [".0", "0.", "0."] => "s",
+        [".0", "00", "0."] => "t",
+        ["0.", "..", "00"] => "u",
+        ["0.", "0.", "00"] => "v",
+        [".0", "00", ".0"] => "w",
+        ["00", "..", "00"] => "x",
+        ["00", ".0", "00"] => "y",
+        ["0.", ".0", "00"] => "z",
+        ["..", "..", ".."] => " ",
+        ["..", "0.", ".."] => ",",
+        ["..", "00", ".0"] => "."
+      }
+
+      expect(translator.alphabet).to eq(expected)
     end
   end
 
